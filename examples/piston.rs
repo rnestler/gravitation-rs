2e4a486a62c9d868014dd9ce7c0c6a33f4aa868b
Raphael Nestler <raphael.nestler@gmail.com>
2016-12-28 16:26:41 +0100

    piston: display cycles per second in draw thread
    The worker thread should be working ;)

--- examples/piston.rs
+++ examples/piston.rs
@@ -7,7 +7,7 @@ use std::sync::{Arc, Mutex};
 use std::sync::mpsc::channel;
 use std::thread;
 
-use piston_window::*;
+use piston_window::{WindowSettings, PistonWindow, clear, ellipse};
 use rand::Rng;
 
 use gravitation::*;
@@ -26,14 +26,20 @@ fn make_world() -> World {
     World::new(SCREEN_WIDTH, SCREEN_HEIGHT, STAR_COUNT, Some(prng_init))
 }
 
+struct Stats {
+    pub cycles_per_s: f64,
+}
+
 fn main() {
     let mut window: PistonWindow =
         WindowSettings::new("Gravitation", [SCREEN_WIDTH, SCREEN_HEIGHT])
         .exit_on_esc(true).fullscreen(true).build().unwrap();
 
+    let stats = Arc::new(Mutex::new(Stats{cycles_per_s: 0.0}));
     let world = Arc::new(Mutex::new(make_world()));
     let (tx, rx) = channel();
     let update_world = world.clone();
+    let stats_clone = stats.clone();
     thread::spawn(move|| {
         loop {
             let start = time::precise_time_s();
@@ -46,7 +52,7 @@ fn main() {
                 *update_world.lock().unwrap() = world_copy;
             }
             let stop = time::precise_time_s();
-            println!("cycle time: {}s/1000", stop-start);
+            stats_clone.lock().unwrap().cycles_per_s = 1000.0 / (stop - start);
         }
     });
 
@@ -65,6 +71,8 @@ fn main() {
                         [star.position.x, star.position.y, size, size],
                         c.transform, g);
             }
+            let cycles_per_s = stats.lock().unwrap().cycles_per_s;
+            println!("Cycles/s: {}", cycles_per_s);
             let threshold = STAR_COUNT / 2;
             if visible_counter < threshold {
                 tx.send(ThreadCommand::Reset).expect("Sending command to worker failed");
